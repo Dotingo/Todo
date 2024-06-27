4b2113c8add61644f2669eb677038ce3b7fcdb26
Dotingo <m9132824@gmail.com>
2024-06-27 12:19:08 +0300

    add lottie

--- app/src/main/java/com/example/todo/presentation/home_screen/components/EmptyTaskScreen.kt
+++ app/src/main/java/com/example/todo/presentation/home_screen/components/EmptyTaskScreen.kt
@@ -1,24 +1,46 @@
 package com.example.todo.presentation.home_screen.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.todo.R
 
 @Composable
 fun EmptyTaskScreen(paddingValues: PaddingValues) {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .padding(paddingValues),
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
         contentAlignment = Alignment.Center
-    ){
+    ) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Задач нет", fontFamily = FontFamily.Monospace, fontSize = 26.sp)
+            val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.cactus))
 
+            LottieAnimation(
+                composition = composition,
+                contentScale = ContentScale.FillWidth
+            )
+        }
     }
 }
